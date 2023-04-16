d6bc14112420a9ddeeeddefd3a2b794c7952c804
CarlosMilano <a01383102@itesm.mx>
2023-04-15 21:59:59 -0600

    Initial Commit

---
+++ IOSHack/IOSHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSHackApp.swift
+//  IOSHack
+//
+//  Created by Carlos Milano on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
