c54359bddda191a8328470740d980b3be576d385
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-15 23:34:36 -0600

    Created Chat View
    the chat view was created as an empty view

---
+++ IOSHack/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  IOSHack
+//
+//  Created by Erick Siller on 15/04/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
