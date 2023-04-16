8a65f51e2ee064e11e581a2d0c99f206ab102246
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-16 03:20:06 -0600

    Chatbot view styles improved

--- IOSHack/ChatView.swift
+++ IOSHack/ChatView.swift
@@ -20,7 +20,7 @@ final class ViewModel: ObservableObject{
     func send( text: String, completion: @escaping (String) -> Void ){
         client?.sendCompletion(with: text, maxTokens: 500, completionHandler: { result in switch result {
         case .success(let model):
-            let output = model.choices?[0].text ?? ""
+            let output = model.choices?.first?.text ?? ""
             completion(output)
         case .failure:
             completion("hubo un error")
@@ -44,16 +44,28 @@ struct ChatView: View {
             Spacer()
             
             HStack{
-                TextField("Escribe aqui...", text: $text )
-                Button("Send"){
+                TextField("¿Cual es tu duda?", text: $text ) .frame(height: 30)
+                    .padding(12)
+                    .font(.system(size: 20, weight: .bold ))
+                    .background(Color.white)
+                    .cornerRadius(8)
+                Button(action: {
                     send()
+                }) {
+                    Image(systemName: "paperplane.fill")
+                        .foregroundColor(Color("MainColor"))
+                        .font(.system(size: 37))
                 }
+                .padding(6)
+                .background(Color("Yellow"))
+                .cornerRadius(8)
             }
         }
         .onAppear{
             viewModel.setup()
         }
         .padding()
+        .background(Color("MainColor"))
     }
     func send(){
         guard !text.trimmingCharacters(in: .whitespaces).isEmpty else{
