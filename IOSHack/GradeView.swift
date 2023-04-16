a385aad1f178810e8cd344b5e7b5aa2261f9b5de
CarlosMilano <a01383102@itesm.mx>
2023-04-16 06:07:47 -0600

    Content view

---
+++ IOSHack/GradeView.swift
@@ -0,0 +1,21 @@
+//
+//  GradeView.swift
+//  IOSHack
+//
+//  Created by Carlos Milano on 16/04/23.
+//
+
+import SwiftUI
+
+struct GradeView: View {
+    var body: some View {
+        Color("MainColor")
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct GradeView_Previews: PreviewProvider {
+    static var previews: some View {
+        GradeView()
+    }
+}
