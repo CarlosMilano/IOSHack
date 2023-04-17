eccee41d8641152cda516976e670705b8c7af8ca
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-16 22:59:42 -0600

    Created Congrats View

---
+++ IOSHack/CongratulationsView.swift
@@ -0,0 +1,20 @@
+//
+//  CongratulationsView.swift
+//  IOSHack
+//
+//  Created by Erick Siller on 16/04/23.
+//
+
+import SwiftUI
+
+struct CongratulationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CongratulationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CongratulationsView()
+    }
+}
