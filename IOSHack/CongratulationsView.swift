15dbaa3f50d6fb2581882050e46c98af8cc6a84b
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-04-16 23:41:56 -0600

    Congrats View Completed and functional
    Congrats View Completed and functional, added the styling

--- IOSHack/CongratulationsView.swift
+++ IOSHack/CongratulationsView.swift
@@ -6,11 +6,93 @@
 //
 
 import SwiftUI
+import ConfettiSwiftUI
 
 struct CongratulationsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    @State private var counter: Int = 0
+    @State var firstTextRender = true
+    @State var isTyping = true
+    @State var isTyping1 = false
+    @State private var renderedText = ""
+    @State private var fullText = "¡Felicidades!\n\n¡Completamos la leccíon de hoy!"
+    @State private var renderedText1 = ""
+    @State private var fullText1 = " ¡Platica con un amigo o familiar acerca de lo que aprendiste!"
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State var timer1 = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+        
+        var body: some View {
+            
+            
+            ZStack {
+                Color("MainColor")
+                    .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center) {
+                    Text(renderedText)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 32, weight: .bold ))
+                                .foregroundColor(.white)
+                                .padding()
+                                .cornerRadius(10)
+                                .onReceive(timer) { _ in
+                                    if isTyping && firstTextRender {
+                                        if  renderedText.count < fullText.count {
+                                            renderedText = String(fullText.prefix(renderedText.count + 1))
+                                        } else {
+                                            timer.upstream.connect().cancel()
+                                            isTyping = false
+                                        }
+                                        
+                                    }
+                                }
+                    Image( isTyping || isTyping1 ? "EllieEyesOpen2" : "EllieEyesOpen")
+                        .resizable()
+                        .frame(width: 271, height: 196)
+                        .padding(20)
+                    Spacer()
+                    Button(action: {
+                        counter += 1
+                        isTyping1 = true
+                        firstTextRender = false
+                        timer1 = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+                    }) {
+                        Text("🎉")
+                            .font(.system(size: 50))
+                    } 
+                    .confettiCannon(counter: $counter)
+                    Text(renderedText1)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 23, weight: .bold ))
+                                .foregroundColor(.white)
+                                .padding()
+                                .cornerRadius(10)
+                                .onReceive(timer1) { _ in
+                                    if isTyping1 && !firstTextRender{
+                                        if  renderedText1.count < fullText1.count {
+                                            renderedText1 = String(fullText1.prefix(renderedText1.count + 1))
+                                        } else {
+                                            timer1.upstream.connect().cancel()
+                                            isTyping1 = false
+                                        }
+                                        
+                                    }
+                                }
+                    Spacer()
+                    Button(action: {
+                                print("¡Hola, mundo!")
+                            }) {
+                                Text("Continuar")
+                                    .font(.system(size: 20, weight: .bold ))
+                                    .padding(.horizontal,50)
+                                    .padding(.vertical)
+                                    .foregroundColor(.black)
+                                    .background(Color("Yellow"))
+                                    .cornerRadius(10)
+                            }
+                        .frame(width: 200, height: 50)
+                }
+                .padding()
+            }
+        }
 }
 
 struct CongratulationsView_Previews: PreviewProvider {
